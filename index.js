176b84ff418d17bdf330c32b662db767a8153e10
jaspercurry <jaspercurry@gmail.com>
2017-03-14 19:32:59 -0400

    updated visuals

--- index.js
+++ index.js
@@ -1,28 +1,93 @@
 $(document).ready(function() {
-  var divs = $('div[id^="content-"]').hide(),
-      i = 0;
 
-      // while (i <= divs.length ) {
-      //
-      //       divs.eq(i).fadeIn(400)
-      //                 .delay(1813)
-      //                 .fadeOut(400);
-      //
-      //       i +=1;
-      // }
 
-  (function cycle() {
 
-    if (i === divs.length) {
-      divs.eq(0).fadeIn(400)
-    }
-      divs.eq(i).fadeIn(400)
-                .delay(1313)
-                .fadeOut(400, cycle);
+var colors = new Array(
+  [60,255,60], //green
+  [255,35,98], //pinkred
+  [45,175,230], //lightblue
+  [255,0,255], //purp
+  [62,35,255], //darkblue
+  [255,128,0]); //orange
 
-      i += 1;
+var step = 0;
+//color table indices for:
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
 
-  })();
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient()
+{
+
+  if ( $===undefined ) return;
+
+var c0_0 = colors[colorIndices[0]];
+var c0_1 = colors[colorIndices[1]];
+var c1_0 = colors[colorIndices[2]];
+var c1_1 = colors[colorIndices[3]];
+
+var istep = 1 - step;
+var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+ $('#gradient').css({
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+  }
+}
+
+setInterval(updateGradient,60);
+
+
+  // var divs = $('div[id^="content-"]').hide(),
+  //     i = 0;
+  //
+  //     // while (i <= divs.length ) {
+  //     //
+  //     //       divs.eq(i).fadeIn(400)
+  //     //                 .delay(1813)
+  //     //                 .fadeOut(400);
+  //     //
+  //     //       i +=1;
+  //     // }
+  //
+  // (function cycle() {
+  //
+  //   if (i === divs.length) {
+  //     divs.eq(0).fadeIn(400)
+  //   }
+  //     divs.eq(i).fadeIn(400)
+  //               .delay(1313)
+  //               .fadeOut(400, cycle);
+  //
+  //     i += 1;
+  //
+  // })();
 
 
 });
